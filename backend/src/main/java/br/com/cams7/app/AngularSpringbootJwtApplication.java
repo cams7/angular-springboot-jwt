e033c44b25570c11e9978b2f69761eb39a485d57
César Magalhães <ceanma@gmail.com>
2019-06-23 10:03:06 -0300

    Foram criados os modulos 'backend' e 'frontend'

---
+++ backend/src/main/java/br/com/cams7/app/AngularSpringbootJwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.cams7.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootJwtApplication.class, args);
+	}
+
+}
